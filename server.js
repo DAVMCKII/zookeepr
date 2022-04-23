55eaa3f6c915b404b1cb7532be803e9163fc7d0d
DAVMCKII <mckdav98@gmail.com>
2022-04-23 17:28:48 -0400

    edited heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -54,6 +55,6 @@ let results = animals;
 
 
 
-app.listen(3001, () => {
-console.log('API server now on port 3001!')
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
